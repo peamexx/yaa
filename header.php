c41410c3428ffc6af124e8188b0993844e4dbcef
Roy King <peamexx@gmail.com>
2019-02-05 17:43:32 +0900

    1

---
+++ header.php
@@ -0,0 +1,25 @@
+<div class="header">
+    <div class="head">
+        <div class="head_inner">
+            <h1><a href=""><img src="https://s.yimg.jp/images/top/sp2/cmn/logo-170307.png" alt="야후재팬 로고"></a></h1>
+            <div class="search">
+                <select name="" id="">
+                    <option value="">画像</option>
+                    <option value="">動画</option>
+                    <option value="">知恵袋</option>
+                    <option value="">地図</option>
+                    <option value="">リアルタイム</option>
+                    <option value="">求人</option>
+                    <option value="">辞書</option>
+                    <option value="">ショッピング</option>
+                    <option value="">レシピ</option>
+                </select>
+                <input type="text">
+                <input type="submit" value="検索">
+            </div>
+            <a href="" class="link_main">야후를 시작페이지로</a>
+        </div>
+    </div>
+    <div class="service">
+    </div>
+</div>
