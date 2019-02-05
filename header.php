2a4fd8e1a6c8d096916a076b51b85164e9203eab
Roy King <peamexx@gmail.com>
2019-02-05 20:26:21 +0900

    1

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
         <div class="head_inner">
             <h1><a href=""><img src="https://s.yimg.jp/images/top/sp2/cmn/logo-170307.png" alt="야후재팬 로고"></a></h1>
             <div class="search">
+                <h2 class="screen_out">검색</h2>
                 <select name="" id="">
                     <option value="">画像</option>
                     <option value="">動画</option>
@@ -17,9 +18,20 @@
                 <input type="text">
                 <input type="submit" value="検索">
             </div>
-            <a href="" class="link_main">야후를 시작페이지로</a>
+            <!--<a href="" class="link_main">야후를 시작페이지로</a>-->
         </div>
     </div>
     <div class="service">
+        <div class="service_inner">
+            <h2 class="screen_out">주요 서비스</h2>
+            <ul>
+                <li><a href=""><img src="https://via.placeholder.com/50x50" alt=""><span>トラベル</span></a></li>
+                <li><a href=""><img src="https://via.placeholder.com/50x50" alt=""><span>オークション</span></a></li>
+                <li><a href=""><img src="https://via.placeholder.com/50x50" alt=""><span>ショッピング</span></a></li>
+                <li><a href=""><img src="https://via.placeholder.com/50x50" alt=""><span>プレゼント</span></a></li>
+                <li><a href=""><img src="https://via.placeholder.com/50x50" alt=""><span>カード</span></a></li>
+                <li><a href=""><img src="https://via.placeholder.com/50x50" alt=""><span>メール</span></a></li>
+            </ul>
+        </div>
     </div>
 </div>
