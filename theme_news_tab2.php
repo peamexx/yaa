94569ec33cf4bc694c6a77c0d3cb0ec979f589d6
Roy King <peamexx@gmail.com>
2019-02-22 18:45:24 +0900

    d

--- theme_news_tab2.php
+++ theme_news_tab2.php
@@ -1,13 +1,13 @@
 <div class="top">
     <a href="">
-        <img src="img/theme_news/top2.jpg" alt="">
+        <img src="img/theme_news/tab2/top.jpg" alt="">
         <h3>スズキ4．9万台リコール＝エンジン始動できない恐れ</h3>
     </a>
     <ul>
-        <li><a href=""><img src="img/theme_news/bottom2-1.jpg" alt="기사사진1"><span>セブン24H 現場軽視の戦略写真</span></a></li>
-        <li><a href=""><img src="https://via.placeholder.com/137x85" alt="기사사진2"><span>和牛受精卵持ち出し 家宅捜索映像</span></a></li>
-        <li><a href=""><img src="img/theme_news/bottom2-3.jpg" alt="기사사진3"><span>東芝メモリに数千億円出資へ写真</span></a></li>
-        <li><a href=""><img src="img/theme_news/bottom2-4.jpg" alt="기사사진4"><span>ソニー来春の採用 技術系強化写真new</span></a></li>
+        <li><a href=""><img src="img/theme_news/tab2/bottom1.jpg" alt=""><span>セブン24H 現場軽視の戦略写真</span></a></li>
+        <li><a href=""><img src="img/theme_news/tab2/bottom2.jpg" alt=""><span>和牛受精卵持ち出し 家宅捜索映像</span></a></li>
+        <li><a href=""><img src="img/theme_news/tab2/bottom3.jpg" alt=""><span>東芝メモリに数千億円出資へ写真</span></a></li>
+        <li><a href=""><img src="img/theme_news/tab2/bottom4.jpg" alt=""><span>ソニー来春の採用 技術系強化写真new</span></a></li>
     </ul>
 </div>
 <div class="bottom">
--- theme_news_tab3.php
+++ theme_news_tab3.php
@@ -1,34 +1,30 @@
 <div class="top">
     <a href="">
-        <img src="https://via.placeholder.com/690x286" alt="">
-        <h3>ACL東地区16クラブが決定！　鹿島は快勝で本戦出場、広島はPK戦の末辛くも勝利</h3>
+        <img src="img/theme_news/tab3/top.jpg" alt="">
+        <h3>可愛すぎる野球女子と話題の坪井ミサト。球速へのこだわりは本物だ</h3>
     </a>
     <ul>
-        <li><a href=""><img src="https://via.placeholder.com/137x85" alt="기사사진1"><span>空自墜落
-                    全戦闘機の訓練中止動画</span></a></li>
-        <li><a href=""><img src="https://via.placeholder.com/137x85" alt="기사사진2"><span>天皇在位30年式典
-                    共産は欠席写真NEW</span></a></li>
-        <li><a href=""><img src="https://via.placeholder.com/137x85" alt="기사사진3"><span>徳川19代目
-                    知事選は出馬せず動画</span></a></li>
-        <li><a href=""><img src="https://via.placeholder.com/137x85" alt="기사사진4"><span>患者にわいせつ
-                    医師に無罪NEW</span></a></li>
+        <li><a href=""><img src="img/theme_news/tab3/bottom1.jpg" alt=""><span>大河「いだてん」、異例の７話分ハイライト番組を２３日放送　秘蔵ＶＴＲも</span></a></li>
+        <li><a href=""><img src="img/theme_news/tab3/bottom2.jpg" alt=""><span>この枠でしかできないことがある――「モテキ」や「ヨシヒコ」のテレ東深夜枠「ドラマ24」、ブレない“核”とドラマの可能性</span></a></li>
+        <li><a href=""><img src="img/theme_news/tab3/bottom3.jpg" alt=""><span>『銀魂』また完結できず…担当編集が謝罪「どうあがいても絶望」　続きはアプリで配信</span></a></li>
+        <li><a href=""><img src="img/theme_news/tab3/bottom4.jpg" alt=""><span>松岡修造の長女・恵さん　父譲りの熱き主演　宝塚音楽学校文化祭の演劇で</span></a></li>
     </ul>
 </div>
 <div class="bottom">
     <div class="left">
         <ul>
-            <li><a href="">徳川19代目 知事選は出馬せず映像new</a></li>
-            <li><a href="">徴用工 現金化動き韓国不介入写真</a></li>
-            <li><a href="">ローソン 駅ナカ売れ筋に変化写真</a></li>
-            <li><a href="">ガガ、2度目の婚約破棄写真new</a></li>
+            <li><a href="">GACKTの豪邸企画　TVで何度も“初公開”されるのはなぜか？/a></li>
+            <li><a href="">ウルルン復活特番 竹内涼真で写真</a></li>
+            <li><a href="">滝沢氏肝入り Jr.サイト開設写真</a></li>
+            <li><a href="">ストーカー女子、その女子をストーカーする男子「君だけは死んでもごめん」1巻</a></li>
         </ul>
     </div>
     <div class="right">
         <ul>
-            <li><a href="">大坂まさかの黒星 ミス連発写真</a></li>
-            <li><a href="">同人誌は高い?説明画像が話題写真</a></li>
-            <li><a href="">消しゴムのカス　床に捨てるのはマナー違反？</a></li>
-            <li><a href="">患者にわいせつ 医師に無罪new</a></li>
+            <li><a href="">夫・浜口がゲストでも…南明奈、ももクロへの純情</a></li>
+            <li><a href="">ＳＫＥ北川綾巴に強力援軍　知立市長が「総選挙」バックアップを約束</a></li>
+            <li><a href="">浜田雅功　勝村政信から「渡哲也感」を指南される！ＭＢＳ「ごぶごぶ」</a></li>
+            <li><a href="">長澤まさみ、“大切なもの”を明かす「限られた時間だと思うので…」</a></li>
         </ul>
     </div>
 </div>
